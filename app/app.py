b942f32e9f2957248861fde04c613bd0fb08a05e
John Clary <john.clary@austintexas.gov>
2018-10-25 12:23:37 -0500

    Set Workers Param

--- app/app.py
+++ app/app.py
@@ -130,5 +130,5 @@ async def trip_handler(request):
     return response.json(trip_features)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True, workers=8)
 
