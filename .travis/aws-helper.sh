7aae153d0e05fa86320fb28d0986a8b389094975
Sergio Garcia <sergiogcx@gmail.com>
2018-11-16 03:10:27 -0600

    Testing Travis Deployment

--- .travis/aws-helper.sh
+++ .travis/aws-helper.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Set these up in Travis next time around
-DOCKLESS_REPO="445025639710.dkr.ecr.us-east-1.amazonaws.com"
-DOCKLESS_CLUSTER="dockless-austintexas-io"
-DOCKLESS_SERVICE="dockless-api-service"
-
-# Also set these dependencies in Travis (Settings > Environment Variables) be sure they are private
-export AWS_DEFAULT_REGION="us-east-1"
-export AWS_ACCESS_KEY_ID="powwow_id_here"
-export AWS_SECRET_ACCESS_KEY="papapaw_secret_here"
-
-
 echo "Logging in to AWS Docker Repository"
 $(aws ecr get-login --no-include-email --region us-east-1)
 
@@ -20,25 +9,35 @@ function build_dockless {
 	DOCKLESS_MODE=":latest"
 	DOCKLESS_FILE="Dockerfile"
 
+	# First, determine if we are building the base image.
 	if [ "${DOCKLESS_IMAGE}" = "base" ]; then
 		DOCKLESS_MODE=":base"
 		DOCKLESS_FILE="${DOCKLESS_FILE}.base"
 	fi;
 
+	# Then, generate the tag string to use in the AWS private registry
 	DOCKLESS_TAG="cityofaustin/dockless-api${DOCKLESS_MODE}"
+	echo "Final Image Name (tag): ${DOCKLESS_TAG}"
 
+	# Build
   echo "Building ${DOCKLESS_FILE} with tag ${DOCKLESS_TAG}"
 	docker build -f $DOCKLESS_FILE -t $DOCKLESS_TAG .
 
+	# Tag
   echo "Tagging ${DOCKLESS_TAG} with tag ${DOCKLESS_TAG}"
 	docker tag $DOCKLESS_TAG $DOCKLESS_REPO/$DOCKLESS_TAG
 
+	# Push
   echo "Pushing: $DOCKLESS_REPO/$DOCKLESS_TAG"
   docker push $DOCKLESS_REPO/$DOCKLESS_TAG
 
   echo "Done"
 }
 
+#
+# Restarts all tasks (containers) in ECS, this will cause them to relaunch
+# with the latest version of the code and/or latest version of the database.
+#
 function restart_all_tasks {
   echo "Rebooting all tasks in ECS"
   aws ecs update-service --force-new-deployment --cluster $DOCKLESS_CLUSTER --service $DOCKLESS_SERVICE
